6594f442ada0b3ccb0bafd1cb1be3e6d2e322a0d
horihiro <hori.kawa.shima.tool+github@gmail.com>
2018-09-15 20:20:28 +0900

    remove semi colon

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
 
-process.argv[2] = 'start'; // set argument to 'start'
-require('nuxt/bin/nuxt');  // execute command 'nuxt
+process.argv[2] = 'start' // set argument to 'start'
+require('nuxt/bin/nuxt') // execute command 'nuxt
