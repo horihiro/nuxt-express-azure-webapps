c57401b319ba3e1cc81bbd1d7335b2be94541b36
horihiro <hori.kawa.shima.tool+github@gmail.com>
2018-09-25 21:08:49 +0900

    set env for NamedPipe

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
 
+process.env.UNIX_SOCKET = process.env.PORT
 process.argv[2] = 'start' // set argument to 'start'
 require('nuxt/bin/nuxt') // execute command 'nuxt
